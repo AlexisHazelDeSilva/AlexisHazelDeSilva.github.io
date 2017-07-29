c447de4e2bfdde07d196346ed5eef7c8e64c49c2
AlexisHazelDeSilva <alexishdesilva@gmail.com>
2017-07-29 11:27:06 -0400

    Minor Bug Fix
    May have had to flip a greater than sign

--- vehicle.js
+++ vehicle.js
@@ -130,7 +130,7 @@ function Vehicle(x,y){
             if(this.dna[2] < this.MIN_PERCEPT){
                 this.dna[2] = this.MIN_PERCEPT;
             }
-            else if(this.dna[2] < this.MAX_PERCEPT){
+            else if(this.dna[2] > this.MAX_PERCEPT){
                 this.dna[2] = this.MAX_PERCEPT;
             }
         }
@@ -140,7 +140,7 @@ function Vehicle(x,y){
             if(this.dna[3] < this.MIN_PERCEPT){
                 this.dna[3] = this.MIN_PERCEPT;
             }
-            else if(this.dna[3] < this.MAX_PERCEPT){
+            else if(this.dna[3] > this.MAX_PERCEPT){
                 this.dna[3] = this.MAX_PERCEPT;
             }
         }
